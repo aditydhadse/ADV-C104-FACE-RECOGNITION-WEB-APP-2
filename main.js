0dd83f5fc5f261ff191b404a7867442b804ab07f
aditydhadse <87891973+aditydhadse@users.noreply.github.com>
2022-02-24 22:36:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.getElementById("result").innerHTML='<img id="captured_image" src="'+dat
 }
 console.log('ml5 version:',ml5.version);
 
-//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/FDAW_5BV8//model.json',modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/FDAW_5BV8//model.json',modelLoaded)
 
 function modelLoaded(){
     console.log("model Loaded")
